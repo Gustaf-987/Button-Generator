2de6eef0e5afb2b2f6ed475628a9aa4e60917578
Gustaf Gonzalez <tony.campoy@gmail.com>
2020-12-31 21:50:08 -0600

    homework update

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-var generateBtn = document.querySelector("#generate");
-const charRange = document.getElementById ("charRange")
-const charNumber = document.getElementById ("charNumber")
-const includeUppercase = document.getElementById ("includeUppercase")
-const includeNumbers = document.getElementById ("includeNumbers")
-const includeSymbols = document.getElementById ("includeSymbols")
-// const card = document.getElementById("passwordForm")
-// const password = document.getElementById("password")
+
+// generateBtn.addEventListener("click", writePassword)
+
+// // // Write password to the #password input
+// function writePassword() {
+//     var password = generatePassword();
+//     var passwordText = document.querySelector("#password");
+  
+//     passwordText.value = password;
+//   }
+
+
+// var generateBtn = document.getElementsByClassName("passwordGeneratorForm")
+const charRange = document.getElementById("charRange")
+const charNumber = document.getElementById("charNumber")
+const form = document.getElementById("passwordGeneratorForm")
+const passwordDisplay = document.getElementById("password")
+const includeUppercaseElement = document.getElementById ("includeUppercase")
+const includeNumbersElement = document.getElementById ("includeNumbers")
+const includeSymbolsElement = document.getElementById ("includeSymbols")
 
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
@@ -18,52 +30,45 @@ const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
      arrayFromLowToHigh(123,126)
     )
 
-
-generateBtn.addEventListener("click", writePassword)
-charNumber.addEventListener("input", syncCharacterAmount)
 charRange.addEventListener("input", syncCharacterAmount)
+charNumber.addEventListener("input", syncCharacterAmount)
 
-function syncCharacterAmount(i) {
-  const value = i.target.value
-  charNumber.value=value
-  charRange.value=value
-}
-
-// On button click, run this function
-function generatePassword(charAmount, includeUppercase, includeNumbers, includeSymbols) {
-    let charCodes = LOWERCASE_CHAR_CODES
-    const charAmount = charNumber.value
+form.addEventListener("submit", e=> {
+    e.preventDefault()
+    const characterAmount = charNumber.value
     const includeUppercase = includeUppercaseElement.checked
     const includeNumbers = includeNumbersElement.checked
     const includeSymbols = includeSymbolsElement.checked
-   if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-   if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
-   if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+    const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+    passwordDisplay.innerText = password
+})
 
-//    math not working
-   const passwordChars = []
-   for (let z= 0; z < charAmount; z++) {
-       const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
-       passwordChars.push(String.fromcharCode(characterCode))
-   }
-   return passwordChars.join("")
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+    let charCodes = LOWERCASE_CHAR_CODES
+    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+    if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+
+    const passwordCharacters =[]
+    for (let i= 0; i < characterAmount; i++) {
+      const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+      passwordCharacters.push(String.fromCharCode(characterCode))}
+
+    return passwordCharacters.join("")     
 }
 
-
-// // Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
-
-//   Loops through possible character choices
 function arrayFromLowToHigh(low, high) {
     const array = []
-    for (let x= low; x <=high; x++) {
-        array.push(x)
+    for (let y= low; y <= high; y++) {
+        array.push(y)
     }
     return array
 }
+
+function syncCharacterAmount(x) {
+    const value = x.target.value
+    charRange.value = value
+    charNumber.value = value
+
+}
+
